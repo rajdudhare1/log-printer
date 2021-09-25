2d80fe7a19513eacaa2aea4424e30de2515360ba
Raj Dudhare <rajdudhare1@gmail.com>
2021-09-25 21:12:02 +0530

    Final Update

--- logger.py
+++ logger.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template,Response,request,jsonify
+from flask import Flask, render_template,Response,request,jsonify,stream_with_context
 from time import sleep
+import requests
 
 APP = Flask(__name__,static_folder="app/static/",template_folder="app/static/")
+DATA= {}
+
+
+
 @APP.route("/", methods=["GET"])
 def root():
     """index page"""
@@ -10,9 +15,9 @@ def root():
 @APP.route("/feeder", methods = ['POST'])
 def feed():
     if request.method == 'POST':
-       content = request.json
-       #print(content)
-       return "Ok"
+       with open('job.log','a') as a:
+           a.write(str((request.json[0]['log']))+"\n")
+    return "OK"
 
 def logs():
     with open("job.log", "r") as f:
@@ -20,15 +25,18 @@ def logs():
                 yield f.read()
                 sleep(1)
 
-def printLogs():
-    pass
+def printLogs(msg=DATA):
+    while True:
+        yield str(msg)
+        sleep(1)
 
 
 @APP.route("/stream", methods=["GET"])
 def stream():
     """returns logging information"""
-    return Response(logs(), mimetype="text/plain", content_type="text/event-stream")
+    return Response(stream_with_context(logs()), mimetype="application/json", content_type="application/stream+json")
 
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=8080, threaded=True)
+    
